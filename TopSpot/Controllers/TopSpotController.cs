6be33a23e33d9be38f20db1b5bcdcf68fa63c10d
Stephen Rosso <stephenprosso@gmail.com>
2017-03-28 11:17:56 -0700

    First Commit

---
+++ TopSpot/Controllers/TopSpotController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.IO;
+
+namespace TopSpot.Controllers
+{
+
+
+	public class TopSpotController : ApiController
+	{
+		[HttpGet, Route("api/getTopSpot")]
+		public IHttpActionResult GetAllTopSpot()
+		{
+			var TopSpot = File.ReadAllText("C:\\Users\\Stephenprosso\\Documents\\Dev\\TopSpot\\TopSpots.json");
+		return Ok(TopSpot);
+		}
+	}
+}
---
+++ TopSpot/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TopSpot.WebApiApplication" Language="C#" %>
